a10d85e795e0666b0b9b5fdac1b63d0f9ec44ce7
camille-codes <63538087+camille-codes@users.noreply.github.com>
2021-01-28 14:53:05 +1100

    Bug fixed
    Now functional

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,31 +1,40 @@
 import random
 from random import shuffle
+from random import randrange
 
 print('Welcome to Secret Santa!\n')
+
 no_of_ppl = input('Enter number of participants: ')
+
 int_no_ppl = int(no_of_ppl) #converts string to integer
 
 print('Enter names of participants:')
+
 #puts names of participants in a list
 list_names = []
 for i in range(int_no_ppl):
     list_names.append(input())
 
-#copies list and assigns to new variable
 list_randomized = list_names[:] 
 
-#using Fisher-Yates algorithm
-for i in range(len(list_randomized)-1, 0, -1):
-    j = random.randint(0, i+1)
-    list_randomized[i], list_randomized[j]= list_randomized[j], list_randomized[i]
+#implementing Satollo Algorithm
+def sattoloCycle(list_randomized):
+    i = len(list_randomized)
+    while i > 1:
+        i = i - 1
+        j = randrange(i)  # 0 <= j <= i-1
+        list_randomized[j], list_randomized[i] = list_randomized[i], list_randomized[j]
+    return list_randomized
+
+list_satollo = sattoloCycle(list_randomized)
 
 input('Press Enter to begin shuffling...')
 
 print('Now give your gift to your partner!')
-list_partners = dict(zip(list_names, list_randomized))
 
+#combines two lists into a dictionary
+list_partners = dict(zip(list_names, list_satollo))
     
-# iterating list_partners dictionary
-# prints dictionary into two columns
+#iterating list_partners dictionary so that two columns show
 for k, v in list_partners.items():
-    print(f'{k:<10} {v}')
+    print(f'{k:<10} --> {v}')
